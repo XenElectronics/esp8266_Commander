8b740a329e100994e20d15ebefdabfbc705c550b
DaniusKalv <danius.kalvaitis@gmail.com>
2016-04-22 15:09:51 +0200

    Added gitignore

--- esp8266Commander/esp8266Commander/Properties/AssemblyInfo.cs
+++ esp8266Commander/esp8266Commander/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyFirstWindowsApplication")]
+[assembly: AssemblyTitle("esp8266Commander")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Licence Owner")]
-[assembly: AssemblyProduct("MyFirstWindowsApplication")]
-[assembly: AssemblyCopyright("Copyright © Licence Owner 2016")]
+[assembly: AssemblyProduct("esp8266Commander")]
+[assembly: AssemblyCopyright("Copyright © Danius Kalvaitis 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
